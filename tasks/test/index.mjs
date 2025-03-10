b9f1c072896047ebcfe8361ad76c0fafd582b566
Jean-Rémy CLIGNY <jrc@jrc-solutions.fr>
2025-03-10 07:33:10 +0100

    Fix minor typos

--- tasks/test/index.mjs
+++ tasks/test/index.mjs
@@ -13,17 +13,17 @@ task('run-spec-tests-with-xunit-reporter', async function () {
     await runSpecTests({ reporter: 'xunit', });
 });
 
-export const run = series(
-    'run-spec-tests',
-    'run-e2e-tests',
-);
-
 task('run-e2e-tests', async function () {
     await runE2ETests();
 });
 task('run-e2e-tests-with-xunit-reporter', async function () {
     await runE2ETests({ reporter: 'xunit', });
 });
+
+export const run = series(
+    'run-spec-tests',
+    'run-e2e-tests',
+);
 
 export const runCI = series(
     'run-spec-tests-with-xunit-reporter',
